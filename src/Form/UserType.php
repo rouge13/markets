0bab804dd5cc8e0ad27d92462d961c7ce48c6e88
Emilie FLick <h2e.flick@gmail.com>
2020-05-21 22:49:04 +0200

    precision des types de champs email, alias et region

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -3,7 +3,9 @@
 namespace App\Form;
 
 use App\Entity\User;
+use Doctrine\DBAL\Types\TextType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
@@ -15,9 +17,9 @@ class UserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('alias')
-            ->add('email')
-            ->add('region')
+            ->add('alias', TextType::class)
+            ->add('email', EmailType::class)
+            ->add('region',TextType::class)
 
             ->add('password' , RepeatedType::class, [
                 'type' => PasswordType::class,
