aa2a780135e9d438687d5eb73bcf34838b778b5f
Emilie FLick <h2e.flick@gmail.com>
2020-05-26 18:56:03 +0200

    ajout fonction delete user, message et market + correction dans page markets

---
+++ src/Controller/DeleteController.php
@@ -0,0 +1,49 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use App\Repository\ContactRepository;
+use App\Repository\MarketRepository;
+use App\Repository\UserRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+
+class DeleteController extends AbstractController
+{
+
+    public function deleteAction(Request $request,$name, $id , MarketRepository $marketRepository , UserRepository $userRepository , ContactRepository $contactRepository){
+
+        $manager = $this->getDoctrine()->getManager();
+
+        switch ($name) {
+            case 'market':
+                $market = $marketRepository->find($id);
+                $manager->remove($market);
+                $manager->flush();
+                return $this->redirectToRoute('home');
+                break;
+
+            case 'user':
+                $user = $userRepository->find($id);
+                $manager->remove($user);
+                $manager->flush();
+                return $this->redirectToRoute('home');
+                break;
+
+            case 'message':
+                $message = $contactRepository->find($id);
+                $manager->remove($message);
+                $manager->flush();
+                return $this->redirectToRoute('home');
+                break;
+
+
+
+        }
+        return $this->redirectToRoute('home');
+
+    }
+
+}
